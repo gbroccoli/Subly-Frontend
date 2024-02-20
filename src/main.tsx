ab43357fba7d47c63a9ff09a353961097870cb65
gbroccoli <vovo.r@ya.ru>
2024-02-20 21:32:49 +1000

    create fonst styles add new plufgins

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './pages/App'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import "./app/font/stylesheet.css"
+import "./index.css"
+
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </React.StrictMode>,
 )
